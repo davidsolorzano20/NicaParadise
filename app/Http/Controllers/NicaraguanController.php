91d6d0e2de42241fba4da13b430651b0482fe90f
Luis Solórzano <luisnic20@gmail.com>
2016-09-25 17:45:51 -0600

    modified homepage an Home

--- app/Http/Controllers/NicaraguanController.php
+++ app/Http/Controllers/NicaraguanController.php
@@ -8,7 +8,7 @@ use App\Http\Requests;
 
 class NicaraguanController extends Controller {
     public function Index() {
-      return view('welcome');
+      return view('welcometo');
     }
 
     public function Home() {
